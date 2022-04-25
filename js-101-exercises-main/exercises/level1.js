67d6004d9746699206d534353b8e8074225fb9e9
Brice Tran <brice1992@msn.com>
2022-04-24 22:53:40 -0400

    commit exercise 1-5

---
+++ js-101-exercises-main/exercises/level1.js
@@ -0,0 +1,47 @@
+function lvl1exercise1 () {
+  // Declare a variable as null and return it!
+  let apple = null
+  return apple
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+  num = 1
+  return num
+}
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  float = 2.03
+  return float
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+  string = "Hello World!"
+  return string
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  array = ["Hello World!", 4]
+  return array
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  object = {key1: "Hello World!", key2: 4}
+  return object
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+  let a = 2
+  let b = 6
+  if (a * b == 11) {
+    return true
+  }
+  else {
+    return false
+  }
+}
---
+++ js-101-exercises-main/exercises/level3.js
@@ -0,0 +1,29 @@
+function lvl3exercise1 () {
+  // Create strings "hello" and a "world", return the concatenation of the two
+  return ("hello" + "world")
+}
+
+function lvl3exercise2 () {
+  // Create a "hello" and a 12, return the concatenation of the two
+  let hello = "hello"
+  let num = 12
+  return (hello + num)
+}
+
+function lvl3exercise3 () {
+  // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+  const clock = 12
+  return ("" + clock)
+}
+
+function lvl3exercise4 () {
+  // Create a "hello world!" string. Return the length of the string
+  let words = "hello world!"
+  return words.length
+}
+
+function lvl3exercise5 () {
+  // Create a "hello world!" string. Return the index of the word "world".
+  let words2 = "hello world"
+  return words2.indexOf("world")
+}
